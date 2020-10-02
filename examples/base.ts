ba0fb92c8a89828b7dc187dbcb7248f5e6d49879
x0rium <tuxera@gmail.com>
2020-10-03 01:17:02 +0300

    fix info

--- examples/base.ts
+++ examples/base.ts
@@ -1,6 +1,6 @@
 import Integration from "../index";
 
-const x = new Integration("040036610");
+const x = new Integration("000036610");
 x.getData().then(data => {
     console.log(data)
 }).catch(e => e);
